9afadaba5103c8eb5566de24dc8dba78f70d7770
raheem paxton <raheemgit>
2021-09-09 13:37:51 -0500

    final push

--- app.py
+++ app.py
@@ -1,34 +1,40 @@
-import numpy as np
-import pymongo
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 
-from flask import Flask, jsonify, render_template
-
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-db = client.MarsDB
-db.info.drop()
+import scrape_mars
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/MarsDB"
+mongo = PyMongo(app)
 
-@app.route('/scrape')
-def scrape():
-    from scrape_mars import scrape
-    data = scrape()
-    
-    return data
 
+
+#route to render index.html template using data from Mongo
 @app.route('/')
 def home():
-    from scrape_mars import scrape
-    data = scrape()
     
-    db.info.insert_one(data)
-    out = list(db.info.find())
-    print(out)
-
+    #find one record of data from the mongo database
+    out = mongo.db.mars.find_one()
     # Return the template with the teams list passed in
-    return render_template('index.html', out=out)
+    #mars goes into the HTML code
+    return render_template('index.html', mars=out)
 
+#Route that will trigger the scrape function
+@app.route('/scrape')
+def scrape():
+    #run the scrape function
+    mars = mongo.db.mars
+    mars_df = scrape_mars.scrape()
+    
+    #update the mongo database useing the updte and upsert=True
+    mars.update({}, mars_df, upsert=True)
+    
+    return redirect('/', code=302)
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
